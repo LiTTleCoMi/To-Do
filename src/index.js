3bef421893701d572f0391f553fc6969e020dad7
Brett Boettcher <hgdxg@littlePC.(none)>
2025-02-25 10:22:32 -0700

    added functionality to toggle visibility of a list's tasks, added functionality to create, edit and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 class Task {
-	constructor(id, taskId = -1, name = "", complete = false) {
+	constructor(id, taskId = -1, name = "Unnamed", complete = false) {
 		this.id = id;
 		this.taskId = taskId;
 		this.name = name;
@@ -11,18 +11,20 @@ class Task {
             <div class="flex items-center gap-x-3">
                 <img class="svg" src="../svgs/menu.svg" alt="reorder drag drop" />
                 <img class="svg" src="../svgs/check_box_outline_blank.svg" alt="mark this task as done" />
-                <div id="${id}-${taskId}-task"><input class="text-xl" type="text" placeholder="Task" /></div>
+                <div id="${this.id}-${this.taskId}-task"><input class="text-xl" type="text" placeholder="Task" /></div>
             </div>
             <div class="flex items-center gap-x-3">
-                <img class="svg" src="../svgs/edit.svg" alt="edit the name of this task" />
-                <img class="svg" src="../svgs/delete.svg" alt="delete this task" />
+                <img onclick="List.updateName(${this.id}, 'span', ${this.taskId})" class="svg" src="../svgs/edit.svg" alt="edit the name of this task" />
+                <img onclick="List.deleteTask(${this.id}, ${this.taskId})" class="svg" src="../svgs/delete.svg" alt="delete this task" />
             </div>
         </div>`;
-		const inputElement = document.getElementById(`${this.id}-${this.taskId}-task`).querySelector("input");
+		const inputElement = document
+			.getElementById(`${this.id}-${this.taskId}-task`)
+			.querySelector("input");
 
 		inputElement.focus();
 		inputElement.addEventListener("blur", () =>
-			List.updateName(id, (taskId = taskId))
+			List.updateName(this.id, "input", this.taskId)
 		);
 	}
 }
@@ -30,10 +32,26 @@ class Task {
 var lists = [];
 
 class List {
-	constructor(id, name = "", tasks = []) {
+	constructor(id, name = "", tasks = [], showTasks = false) {
 		this.id = id;
 		this.name = name;
 		this.tasks = tasks;
+		this.showTasks = showTasks;
+	}
+
+	static deleteTask(id, taskId) {
+        let list = lists.find((d) => d.id == id);
+		list.tasks = list.tasks.filter((task) => {
+			return task.taskId != taskId;
+        });
+        this.loadLists()
+	}
+
+	static toggleTasks(id) {
+		let list = lists.find((i) => i.id == id);
+		console.log(list);
+		list.showTasks = !list.showTasks;
+		List.loadLists();
 	}
 
 	static createList() {
@@ -45,7 +63,7 @@ class List {
         <div id=${id} class="border-2 border-black rounded-4xl w-9/10 overflow-hidden flex flex-col items-center">
             <div id="${id}-list" class="px-7 py-3 h-14 flex items-center justify-between w-full">
                 <div class="flex gap-x-3 items-center">
-                    <img class="svg" src="../svgs/arrow_drop_down.svg" alt="expand to view tasks" />
+                    <img onclick="List.toggleTasks(${id})" class="svg" src="../svgs/arrow_drop_down.svg" alt="expand to view tasks" />
                     <div id="${id}-name"><input type="text" class="text-2xl" placeholder="List Name" /></div>
                 </div>
                 <div class="flex justify-center gap-x-3">
@@ -65,26 +83,43 @@ class List {
 
 	static updateName(id, setting = "input", taskId = "") {
 		let element, item;
-		if (!taskId) {
+		if (taskId === "") {
+			console.log("list");
 			element = document.getElementById(`${id}-name`);
 			item = lists.find((thisItem) => thisItem.id === id);
 		} else {
+			console.log(taskId);
+			console.log("task");
 			element = document.getElementById(`${id}-${taskId}-task`);
+			console.log(id);
 			item = lists
-				.find((thisItem) => thisItem.id === id)
-				.find((t) => t.taskId === taskId);
+				.find((thisItem) => thisItem.id == id)
+				.tasks.find((t) => t.taskId === taskId);
+			console.log(item);
 		}
 
 		if (setting === "input") {
+			console.log("input");
 			const inputElement = element.querySelector("input");
+			console.log(element.innerHTML);
 			item.name = inputElement.value === "" ? "Unnamed" : inputElement.value;
 			this.loadLists();
 		} else if (setting === "span") {
-			element.innerHTML = `<input type="text" class="text-2xl" placeholder="List Name" />`;
+			console.log("span");
+			element.innerHTML = `<input type="text" class="text-${
+				taskId === "" ? "2xl" : "xl"
+			}" placeholder="List Name" />`;
 			const inputElement = element.querySelector("input");
 			inputElement.value = item.name;
 			inputElement.focus();
-			inputElement.addEventListener("blur", () => this.updateName(id));
+
+			if (taskId === "") {
+				inputElement.addEventListener("blur", () => this.updateName(id));
+			} else {
+				inputElement.addEventListener("blur", () =>
+					this.updateName(id, "input", taskId)
+				);
+			}
 		}
 	}
 
@@ -96,31 +131,13 @@ class List {
 
 	static createTask(id) {
 		const list = lists.find((item) => item.id === id);
+		list.showTasks = true;
 		console.log(list);
 		list.tasks.push(new Task(id));
 	}
 
-	static loadTasks(id) {
-		const tasksElement = document.getElementById(id);
-		const list = lists.find((item) => item.id === id);
-		list.tasks.forEach((task, i) => {
-			task.taskId = i;
-			tasksElement.innerHTML += `
-            <div id="${id}-${i}" class="flex justify-between py-1 px-3 border-2 border-black w-9/10 mb-3 rounded-2xl h-12">
-                <div class="flex items-center gap-x-3">
-                    <img class="svg" src="../svgs/menu.svg" alt="reorder drag drop" />
-                    <img id="${id}-${i}-task-completion" class="svg" src="../svgs/check_box_outline_blank.svg" alt="mark this task as done" />
-                    <div id="${id}-${i}-task"><span class="text-xl">${task.name}</span></div>
-                </div>
-                <div class="flex items-center gap-x-3">
-                    <img class="svg" src="../svgs/edit.svg" alt="edit the name of this task" />
-                    <img class="svg" src="../svgs/delete.svg" alt="delete this task" />
-                </div>
-            </div>`;
-		});
-	}
-
 	static loadLists() {
+		console.log("Loading...");
 		const listsElement = document.getElementById("lists-container");
 		listsElement.innerHTML = "";
 		lists.forEach((list) => {
@@ -128,7 +145,7 @@ class List {
             <div id=${list.id} class="border-2 border-black rounded-4xl w-9/10 overflow-hidden flex flex-col items-center">
                 <div id="${list.id}-list" class="px-7 py-3 h-14 flex items-center justify-between w-full">
                     <div class="flex gap-x-3 items-center">
-                        <img onclick="List.loadTasks(${list.id})" class="svg" src="../svgs/arrow_drop_down.svg" alt="expand to view tasks" />
+                        <img  onclick="List.toggleTasks(${list.id})" class="svg" src="../svgs/arrow_drop_down.svg" alt="expand to view tasks" />
                         <div id="${list.id}-name"><span class="text-2xl">${list.name}</span></div>
                     </div>
                     <div class="flex justify-center gap-x-3">
@@ -138,6 +155,24 @@ class List {
                     </div>
                 </div>
             </div>`;
+			if (list.showTasks) {
+				const tasksElement = document.getElementById(list.id);
+				list.tasks.forEach((task, i) => {
+					task.taskId = i;
+					tasksElement.innerHTML += `
+                    <div id="${list.id}-${i}" class="flex justify-between py-1 px-3 border-2 border-black w-9/10 mb-3 rounded-2xl h-12">
+                        <div class="flex items-center gap-x-3">
+                            <img class="svg" src="../svgs/menu.svg" alt="reorder drag drop" />
+                            <img id="${list.id}-${i}-task-completion" class="svg" src="../svgs/check_box_outline_blank.svg" alt="mark this task as done" />
+                            <div id="${list.id}-${i}-task"><span class="text-xl">${task.name}</span></div>
+                        </div>
+                        <div class="flex items-center gap-x-3">
+                            <img onclick="List.updateName(${list.id}, 'span', ${task.taskId})" class="svg" src="../svgs/edit.svg" alt="edit the name of this task" />
+                            <img onclick="List.deleteTask(${list.id}, ${task.taskId})" class="svg" src="../svgs/delete.svg" alt="delete this task" />
+                        </div>
+                    </div>`;
+				});
+			}
 		});
 	}
 }
